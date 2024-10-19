d03de631e35afb8a4993f17426ad1b5c8d5c508e
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2024-10-19 21:23:27 +0900

    仕上げ

--- src/main.rs
+++ src/main.rs
@@ -12,33 +12,42 @@ fn main() {
         // https://bit.ly/4dRtcSP
         .gen_range(1..101);
 
-    println!("秘密の数字: {}", secret_number);
+    // Debug用
+    // println!("秘密の数字: {}", secret_number);
 
-    println!("予想を入力してください");
+    loop {
+        println!("予想を入力してください");
 
-    let mut guess = String::new();
-    io::stdin()
-        // とりあえず知っておいてほしいのは、変数のように参照もデフォルトで不変であることです。
-        // したがって、&guessではなく&mut guessと書いて可変にする必要があります。
+        let mut guess = String::new();
+        io::stdin()
+            // とりあえず知っておいてほしいのは、変数のように参照もデフォルトで不変であることです。
+            // したがって、&guessではなく&mut guessと書いて可変にする必要があります。
+            //
+            // https://bit.ly/4eP5NTo
+            //
+            // 感想: 参照の不変性がイマイチピンと来ない。安全っぽくはある。
+            .read_line(&mut guess)
+            .expect("読み込みに失敗しました");
+
+        // Rustではguessの前の値を新しい値で覆い隠す（shadowする）ことが許されているのです。
+        // この機能はある型から別の型に値を変換するときによく使われる
+        // https://bit.ly/3YvlY2v
         //
-        // https://bit.ly/4eP5NTo
-        //
-        // 感想: 参照の不変性がイマイチピンと来ない。安全っぽくはある。
-        .read_line(&mut guess)
-        .expect("読み込みに失敗しました");
+        // 感想: キモ。
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    // Rustではguessの前の値を新しい値で覆い隠す（shadowする）ことが許されているのです。
-    // この機能はある型から別の型に値を変換するときによく使われる
-    // https://bit.ly/3YvlY2v
-    //
-    // 感想: キモ。
-    let guess: u32 = guess.trim().parse().expect("数字を入力してください");
+        println!("あなたの予想: {}", guess);
 
-    println!("あなたの予想: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("小さい！"),
-        Ordering::Greater => println!("大きい！"),
-        Ordering::Equal => println!("正解！"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さい！"),
+            Ordering::Greater => println!("大きい！"),
+            Ordering::Equal => {
+                println!("正解！");
+                break;
+            }
+        }
     }
 }
