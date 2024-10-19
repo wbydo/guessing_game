943893a052aa34c169d629cda264b964a1947840
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2024-10-19 21:07:40 +0900

    random

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("数を予想してください");
+
+    let secret_number = rand::thread_rng()
+        // 下限値は含みますが上限値は含みません。
+        // これと同等の1..=100という範囲を渡すこともできます。
+        //
+        // https://bit.ly/4dRtcSP
+        .gen_range(1..101);
+
+    println!("秘密の数字: {}", secret_number);
 
     println!("予想を入力してください");
 
