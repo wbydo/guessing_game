7b84e18b7d3a98feab8d1410a3d10fa327f2f252
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2024-10-19 20:58:14 +0900

    add comment & lint

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,14 @@ fn main() {
     let mut guess = String::new();
 
     io::stdin()
-      .read_line(&mut guess)
-      .expect("読み込みに失敗しました");
-
+        // とりあえず知っておいてほしいのは、変数のように参照もデフォルトで不変であることです。
+        // したがって、&guessではなく&mut guessと書いて可変にする必要があります。
+        //
+        // https://bit.ly/4eP5NTo
+        //
+        // 感想: 参照の不変性がイマイチピンと来ない。安全っぽくはある。
+        .read_line(&mut guess)
+        .expect("読み込みに失敗しました");
 
     println!("あなたの予想: {}", guess);
 }
